2ebf61630ff4a5d10e3470496bcac99586a986db
tnishino <niche240tamaki@gmail.com>
2021-07-08 10:14:18 +0900

    new nn

--- dlxem/networks.py
+++ dlxem/networks.py
@@ -23,3 +23,30 @@ def get_dnn(input_dim, output_dim):
     model.compile(Adam(lr=1e-3), loss="mean_squared_error")
 
     return model
+
+def get_dnn2(input_dim, output_dim):
+    model = Sequential()
+    model.add(Dense(4096, activation='relu', input_dim=input_dim))
+    model.add(Dropout(0.2))
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(output_dim))
+
+    model.compile(Adam(lr=1e-3), loss="mean_squared_error")
+
+    return model
